0b8ab064644c6253f8d06d9c32453510861d80fe
Mexson Fernandes <robomex2020@gmail.com>
2020-04-14 00:38:27 +0530

    add GET method handler

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const axios = require('axios');
+
 
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: 5050,
         host: 'localhost'
+    });
+
+    server.route({
+        method: ['GET'],
+        path: '/{any*}',
+        handler: async (request, h) => {
+            const url = request.url.href.replace(request.url.origin, '').substr(1,)
+            console.log(url)
+
+            if (request.route.method == 'get') {
+                console.log('dfd')
+                return await axios.get(url)
+                    .then(response => {
+                        const data = {
+                            "statusCode": response.status,
+                            "data": response.data
+                        }
+                        return h.response(data).code(500)
+                    }).catch(error => {
+                        const data = {
+                            "statusCode": 404,
+                            "message": error.Error
+                        }
+                        return h.response(data).code(404)
+                    })
+            }
+        }
     });
 
     await server.start();
